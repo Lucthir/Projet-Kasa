2dde7a3937f698c1718f7fea0556f1651106435e
Lucthir <lucas.thiron@gmail.com>
2023-06-21 16:06:45 +0200

    fix1 host position

--- src/index.js
+++ src/index.js
@@ -1,46 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./utils/style/style.scss";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Error from "./components/Error";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import Logement from "./components/Logement";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Card from "./components/Card";
-// import { useState } from "react";
+
 import Routing from "./routes/Routing";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <Routing />
-  // <React.StrictMode>
-  //   <Router>
-  //     <Header />
-  //     <Routes>
-  //       <Route
-  //         path="/"
-  //         element={
-  //           <Home
-  //             selectedLogement={selectedLogement}
-  //             setSelectedLogement={setSelectedLogement}
-  //           />
-  //         }
-  //       />
-  //       <Route path="/about" element={<About />} />
-  //       <Route
-  //         path={`/logement/${logement.id}`}
-  //         element={<Logement id={logement.id} />}
-  //       />
-  //       {/* <Route path="*" element={<Error />} /> */}
-  //     </Routes>
-  //     <Footer />
-  //   </Router>
-  // </React.StrictMode>
-);
-
-// http://localhost:3000/fiche/b9123946
-
-/* <Route key={fiche.id} path={`/fiche/${fiche.id}`} element={<Fiche id={fiche.id} />} /> */
+root.render(<Routing />);
